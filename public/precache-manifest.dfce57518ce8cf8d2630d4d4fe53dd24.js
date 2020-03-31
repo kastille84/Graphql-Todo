c3fda5cf7d03d910ff2f7f841ef2474d8ca05b90
Edwin <Edwin.Martinez@people10.com>
2020-03-31 15:32:41 -0400

    added public folder at root, which is the build of the frontend which the server looks for

---
+++ public/precache-manifest.dfce57518ce8cf8d2630d4d4fe53dd24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1314e1b3631ff8091930853a706cb1be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "855fdf01b8dc398d6538",
+    "url": "/static/css/main.27979856.chunk.css"
+  },
+  {
+    "revision": "25dcf892a141b2a42155",
+    "url": "/static/js/2.d0ba8112.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/static/js/2.d0ba8112.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "855fdf01b8dc398d6538",
+    "url": "/static/js/main.ef52d56c.chunk.js"
+  },
+  {
+    "revision": "4769b01184c635b2d917",
+    "url": "/static/js/runtime-main.aa4083ec.js"
+  }
+]);
